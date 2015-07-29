b3ab02f7e76848102dca8273a0bda69b93b6cf50
Greg Cardoni <greg@cardoni.net>
2015-07-29 13:06:31 -0700

    updates express "app.configure()" calls

--- app/app.js
+++ app/app.js
@@ -1,28 +1,25 @@
-var express = require("express");
+'use strict';
+
+var express = require( 'express' );
 var app = express();
 
 // Set up the dictionary
-var dictionary;
-var Dictionary = require("../lib/dictionary");
-
-app.configure('production', function() {
-	dictionary = new Dictionary({
-		file: __dirname + '/../db/dictionary.json'
-	});
-});
-
-app.configure('test', function() {
-	dictionary = new Dictionary({
-		file: __dirname + '/../test/test_dictionary.json'
-	});
-});
+var Dictionary = require( '../lib/dictionary' );
+var env = process.env.NODE_ENV || 'production';
+var path = {
+    'test': '/../test/test_dictionary.json',
+    'production': '/../db/dictionary.json'
+};
+var dictionary = new Dictionary( {
+    file: __dirname + path[ env ]
+} );
 
 // Routes
-app.get('/', require("./route-index"));
-app.get('/hello', require("./route-hello"));
-app.get('/lookup', require("./route-lookup")(dictionary));
-app.get('/add', require("./route-add")(dictionary));
-app.get('/remove', require("./route-remove")(dictionary));
+app.get( '/', require( './route-index' ) );
+app.get( '/hello', require( './route-hello' ) );
+app.get( '/lookup', require( './route-lookup' )( dictionary ) );
+app.get( '/add', require( './route-add' )( dictionary ) );
+app.get( '/remove', require( './route-remove' )( dictionary ) );
 
 
 module.exports = app;
